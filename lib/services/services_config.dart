a0c397cfff747265f1b09ad7423d1d15c316fa15
Cristian Ramirez <cristiandramirezm.2024@gmail.com>
2023-01-23 21:21:40 -0500

    Services config as providers for independency injection

---
+++ lib/services/services_config.dart
@@ -0,0 +1,32 @@
+import 'package:cobro_app/services/authentication/authentication_service.dart';
+import 'package:cobro_app/services/theme/theme_service.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
+
+class ServicesConfig extends StatelessWidget {
+  const ServicesConfig({
+    super.key,
+    this.child,
+  });
+  final Widget? child;
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider(
+          create: (context) => GoogleSignIn(),
+        ),
+        Provider(
+          create: (context) => ThemeService(),
+        ),
+        Provider(
+          create: (context) => AuthenticationService(
+            google: Provider.of<GoogleSignIn>(context, listen: false),
+          ),
+        ),
+      ],
+      child: child,
+    );
+  }
+}
